735da9477540efe5c6f5ba3a1c674316de5daee7
OS Kernel <mihailovicmaksim@gmail.com>
2024-09-17 14:01:55 -0700

    mods finished

--- project-base-v1.1/h/Resource.hpp
+++ project-base-v1.1/h/Resource.hpp
@@ -6,13 +6,39 @@
 #define PROJECT_BASE_V1_1_RESOURCE_HPP
 
 
+#include "syscall_cpp.hpp"
+
 class Resource {
 public:
-    Resource(int res) : res(res), threads(0) {}
+    explicit Resource(int res) : res(res), threads(0), mutex(1), waiter(0) {}
+
+    void take(int n) {
+        mutex.wait();
+        while (res < n) {
+            threads++;
+            mutex.signal();
+            waiter.wait();
+            mutex.wait();
+        }
+        res -= n;
+        mutex.signal();
+    }
+
+    void give_back(int n) {
+        mutex.wait();
+        res += n;
+        if (threads > 0)
+            for (int i = 0; i < threads; i++)
+                waiter.signal();
+        threads = 0;
+        mutex.signal();
+    }
 
 private:
     int res;
     int threads;
+    Semaphore mutex;
+    Semaphore waiter;
 };
 
 
